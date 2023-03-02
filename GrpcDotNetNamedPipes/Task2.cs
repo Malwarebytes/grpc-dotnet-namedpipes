bd50182fa69d6ef041187736fed8630faf1b7981
Hernan Martinez <hmartinez@malwarebytes.com>
2023-03-02 09:34:41 -0700

    Add helper class for undefined Task members

---
+++ GrpcDotNetNamedPipes/Task2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcDotNetNamedPipes
+{
+    internal class Task2
+    {
+        private static Task s_completedTask;
+
+        public static Task CompletedTask
+        {
+            get
+            {
+                var completedTask = s_completedTask;
+                if (completedTask == null)
+                    s_completedTask = Task.Delay(0);
+                return completedTask;
+            }
+        }
+        
+        public static Task FromCanceled(CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                throw new ArgumentOutOfRangeException("cancellationToken");
+            }
+
+            return Task.Delay(Timeout.Infinite, cancellationToken);
+        }
+    }
+    
+    internal class Task2<TResult>
+    {
+        public static Task<TResult> FromCanceled(CancellationToken _)
+        {
+            var tcs = new TaskCompletionSource<TResult>(TaskCreationOptions.None);
+            tcs.SetCanceled();
+            return tcs.Task;
+        }
+    }
+}
