3c881b95192539110f6d4d108d490156b0ef6410
Hernan Martinez <hmartinez@malwarebytes.com>
2023-03-16 00:32:27 -0600

    Add demo Server and Client

---
+++ DemoApps/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using EADomain;
+using GrpcDotNetNamedPipes;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new NamedPipeChannel(".", "Malwarebytes.PipeServerName");
+            var client = new Downloader.DownloaderClient(channel);
+
+            var request = new DownloadRequest
+            {
+                Url = "url",
+                AutoRedirect = true
+            };
+
+            var response = await client.DownloadFileAsync(request);
+
+            Console.WriteLine($"Response: {response.FilePath}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DemoApps/Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EADomain;
+using GrpcDotNetNamedPipes;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new NamedPipeServer("Malwarebytes.PipeServerName"))
+            {
+                Downloader.BindService(server.ServiceBinder, new DownloaderService());
+                server.Start();
+                Console.WriteLine("Press key to exit server...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
